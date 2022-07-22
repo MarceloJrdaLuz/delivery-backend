91abc2fb35e34e4329730ef1579c2e09b1c5ade2
Eduardo Dallmann <eduardo.dallmann@ambevtech.com.br>
2022-07-22 10:44:41 -0300

    alterações

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,17 @@
+const path = require("path");
+const { NODE_ENV = "production" } = process.env;
+const nodeExternals = require("webpack-node-externals");
+
 module.exports = {
-    entry: './src/server.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module:{
-        rules: [
-            {
-                test: /\js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        fallback: { 
-            "path": require.resolve("path-browserify"),
-            "url": require.resolve("url/"),
-            "util": require.resolve("util/"),
-            "stream": require.resolve("stream-browserify"),
-            "crypto": require.resolve("crypto-browserify"),
-            "zlib": require.resolve("browserify-zlib"),
-            "os": require.resolve("os-browserify/browser"),
-            "https": require.resolve("https-browserify"),
-            "assert": require.resolve("assert/"),
-            "http": require.resolve("stream-http"),
-            "constants": require.resolve("constants-browserify"),
-        },
-        extensions: ['.js','.ts'],
-     }
-}
+  entry: "./src/server.js",
+  mode: NODE_ENV,
+  target: "node",
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+};
